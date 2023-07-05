f8d59ad1cdb56903c80e7e4cfddaefe1bbfaac4c
Radosław <brzezinskiradek@gmail.com>
2023-07-05 23:53:37 +0200

    design about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,20 +5,16 @@ import Photo2 from '../herbs-g96e2f6537_1280.jpg';
 const About = () => {
 	return (
 		<div className='about'>
-			<div className='about_about'>
-				<div>
+			<div className='about_item'>
                 <h2>O nas</h2>
 				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nisi
-					voluptate quod deleniti recusandae nesciunt sed exercitationem error.
-					Harum laborum reprehenderit eius alias, quis amet quae magni.
-					Voluptatibus, corrupti laudantium.
+				Nasz sklep z ciastami to owoc wieloletniej tradycji cukierniczej. Przez lata pielęgnowaliśmy tajemnicze przepisy i doskonaliliśmy sztukę wypieku. Każde ciasto to ukoronowanie naszej pasji i oddania. Przyjdź i skosztuj smaku historii, którą starannie wpleśliśmy w każdy kęs. Zapraszamy do odkrywania naszych słodkich korzeni!
 				</p>
-				</div>
+
                 <img src={Photo1}></img>
 			</div>
-			<div className='about_ingredients'>
-            <div>
+			<div className='about_item'>
+
 				<h2>Najwyższej jakości składniki</h2>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nisi
@@ -26,7 +22,6 @@ const About = () => {
 					Harum laborum reprehenderit eius alias, quis amet quae magni.
 					Voluptatibus, corrupti laudantium.
 				</p>
-                </div>
                 <img src={Photo2}></img>
 			</div>
 		</div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from "react-router-dom";
 import './header.scss';
 import Arrow from '../Arrow 1.png'
 
@@ -8,12 +9,18 @@ const Header = () => {
 
     const [userContext, setUserContext] = useContext<any>(UserContext);
 
+    const navigate = useNavigate();
+
+    const button = () => {
+        navigate("/products");
+    }
+
 	return (
 		<div className='header'>
             <div className='header_shadow'></div>
             <h1 className='header_text'>"Słodkie chwile, które rozpieszczają zmysły!"</h1>
             <h1 style={{color: 'white', zIndex: '100'}}>{userContext?.providerData ? `Witaj ${userContext.providerData[0].displayName}!` : null }</h1>
-            <button className='header_button'>Sprawdź ofertę <img src={Arrow}></img></button>
+            <button onClick={button} className='header_button'>Sprawdź ofertę <img src={Arrow}></img></button>
         </div>
 	);
 };
